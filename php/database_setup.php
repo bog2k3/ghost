11a16edbf8a64bb2680c3bca3b5ac6c6ce2056b0
Dan Marin <dan@wks1-plt.site>
2015-10-02 14:17:10 +0300

    fix php

--- php/database_setup.php
+++ php/database_setup.php
@@ -1,10 +1,9 @@
 <?php
 
-//$servername = "192.168.0.3";
-$servername = "86.125.49.222";
-$username = "ghost";
-$password = "D58jKe3TZJgXaeYTp9DCF4Kx";
-$database = "ghost";
-$database_port = 46466;
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
+$database_port = 0;
 
 ?>
