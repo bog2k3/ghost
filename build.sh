338b1275142e1a73edd0cf520d16b0b6c7c1cfb6
bogdan ionita <bog2k3@gmail.com>
2015-12-08 21:45:24 +0200

    optimize build script

--- build.sh
+++ build.sh
@@ -3,23 +3,23 @@
 echo Building \"common\"...
 cd common/Release
 make clean
-make all
+make -j4 all
 echo Building \"trad\"...
 cd ../../trad/Release
 make clean 
-make all
+make -j4 all
 echo Building \"maimutza\"...
 cd ../../maimutza/Release
 make clean
-make all
+make -j4 all
 echo Building \"cupid\"...
 cd ../../cupid/Release
 make clean
-make all
+make -j4 all
 echo Building \"watchdog\"...
 cd ../../watchdog/Release
 make clean
-make all
+make -j4 all
 
 echo "Done. Enjoy! :-)  [maimutza prietenoasa]"
 
